65cd52f4625c14823f4737a1d105521efff10d58
ozgun.dogan <ozgun.dogan@prestijyazilim.com.tr>
2022-10-18 21:34:58 +0300

    About PersonList, PersonSave, PersonDelete vs. metods added for PersonBookUI.

---
+++ PhoneBookUI.Test/Models/CreatePersonCommandTestData.cs
@@ -0,0 +1,21 @@
+﻿using ContactService.Application.Persons.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookUI.Test.Models
+{
+    public class CreatePersonCommandTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { new CreatePersonCommand { Name="Ayşe",Surname="Demir",Company="Rise"} };
+            //yield return new object[] { new CreatePersonCommand { Name = "Elif", Surname = "Gür", Company = "Rise" } };
+
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ PhoneBookUI.Test/Models/DeletePersonCommandTestData.cs
@@ -0,0 +1,19 @@
+﻿using ContactService.Application.Persons.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBookUI.Test.Models
+{
+    public class DeletePersonCommandTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1,2 };
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
